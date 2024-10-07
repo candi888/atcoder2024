6d63550d22780a28625cb482454776e5cb4c9e77
candi888 <cdi89724@gmail.com>
2024-10-07 22:50:12 +0900

    tmp

---
+++ abc368/A/main.py
@@ -0,0 +1,34 @@
+"""
+------------------------------------------------------------------------------
+AtCoder用テンプレート
+"""
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, permutations, product
+from sys import setrecursionlimit, stdin
+from typing import Dict, List, Set
+
+
+def input() -> str:
+    return stdin.readline().strip()
+
+
+# setrecursionlimit(700000); import pypyjit; pypyjit.set_param('max_unroll_recursion=-1')
+
+INF = (1 << 61) - 1
+MOD9, MOD10 = 998244353, 1000000007
+"------------------------------------------------------------------------------"
+
+
+def main() -> None:
+    n, k = list(map(int, input().split()))
+    a = list(map(int, input().split()))
+
+    print(*(a[n - k :] + a[: n - k]))
+    return
+
+
+if __name__ == "__main__":
+    main()
