6ef8207b4d4ba33b52f9bd04a145cf59c4218d2f
candi888 <cdi89724@gmail.com>
2024-10-15 01:53:14 +0900

    acc ojを設定 ゆえにmake_for_contest.pyは使わなさそう

---
+++ abc346/a/main.py
@@ -0,0 +1,37 @@
+"""
+--------------------------------------------------------------------------------------------------------------------------------
+AtCoder用テンプレート
+"""
+
+import math
+import operator
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from functools import reduce
+from heapq import heapify, heappop, heappush
+from itertools import accumulate, permutations, product
+from sys import setrecursionlimit, stdin
+
+
+def input() -> str:
+    return stdin.readline().strip()
+
+
+# setrecursionlimit(700000); import pypyjit; pypyjit.set_param('max_unroll_recursion=-1')
+
+INF = (1 << 61) - 1
+MOD9, MOD10 = 998244353, 1000000007
+"-------------------------------------------------------------------------------------------------------------------------------"
+
+
+def main() -> None:
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    print(*[a[i] * a[i + 1] for i in range(n - 1)])
+
+    return
+
+
+if __name__ == "__main__":
+    main()
